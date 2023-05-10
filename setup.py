8b850b4a88581b80250262d1289421d40bd2917b
MohamedFarag21 <13p5066@eng.asu.edu.eg>
2023-05-10 17:36:48 +0200

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tackling Diabetic Retinopathy bu utilizing Deep Learning',
+    author='Mohamed Ibrahim',
+    license='MIT',
+)
